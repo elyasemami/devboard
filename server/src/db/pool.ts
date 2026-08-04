8771f2c2a960326ce63858a5fddd17d92b0c5ab7
elyas <elyas@atozflow.com>
2026-08-04 06:37:36 -0700

    fixed pool.ts and added repositories, etc

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,10 +1,11 @@
 // src/db/pool.ts
+import "dotenv/config";
 import pg from "pg";
 
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   max: 10,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 5_000,
