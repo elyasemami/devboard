e4300d77683b2b90974d705e70a6696048ecd652
elyas <elyas@atozflow.com>
2026-08-06 05:34:37 -0700

    migrations and database schema has been updated

--- server/db/migrations/20260804144834_users_table.sql
+++ server/db/migrations/20260804144834_users_table.sql
@@ -1,3 +1,33 @@
 -- migrate:up
+CREATE TYPE project_status as ENUM ('active', 'inactive', 'completed', 'cancelled');
+
+CREATE TYPE user_status as ENUM ('available', 'busy', 'scheduled');
+
+CREATE TYPE task_status as ENUM ('assigned', )
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_At TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+ );
+
+CREATE TABLE user_profiles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    user_id UUID NOT NULL UNIQUE REFERENCES users ON DELETE CASCADE,
+    avatar_url TEXT,
+    bio TEXT,
+    phone TEXT,
+    created_at TIMESTAMP
+    WITH
+        TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        updated_At TIMESTAMP
+    WITH
+        TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE
 
 -- migrate:down
