4de4212f106c02a69ef16b211bc8633c7e7f9105
elyas <elyas@atozflow.com>
2026-08-09 15:22:12 -0700

    login section is added

--- server/src/auth/auth.routes.ts
+++ server/src/auth/auth.routes.ts
@@ -2,9 +2,9 @@
 import { Router } from "express";
 import { uptime } from "node:process";
 import { z } from "zod";
-import { register } from "./auth.service.ts";
+import { login, register } from "./auth.service.ts";
 
-const credentialsSchema = z.object({
+const masterSchema = z.object({
   email: z.email(),
   password: z.string().min(8).max(256),
   fullName: z.string().min(1).max(200),
@@ -13,7 +13,7 @@ const credentialsSchema = z.object({
 const router = Router();
 
 router.post("/register", async (req, res) => {
-  const parsed = credentialsSchema.safeParse(req.body);
+  const parsed = masterSchema.safeParse(req.body);
   if (!parsed.success)
     return res.status(400).json({ error: parsed.error.flatten() });
   const session = await register(
@@ -21,7 +21,21 @@ router.post("/register", async (req, res) => {
     parsed.data.password,
     parsed.data.fullName,
   );
-  res.status(201).json({ ok: true, sessionId: session.id });
+  res.status(201).json({ message: "Registered" });
+});
+
+router.post("/login", async (req, res) => {
+  const loginSchema = masterSchema.pick({
+    email: true,
+    password: true,
+  });
+  const parsed = loginSchema.safeParse(req.body);
+  if (!parsed.success)
+    return res.status(400).json({ error: parsed.error.flatten() });
+  const { email, password } = parsed.data;
+  const session = await login(email, password);
+  if (!session) throw new Error("There was a problem!");
+  res.status(201).json({ message: "Login Successfull!" });
 });
 
 export default router;
