99261782e54be719f6eeed7939a159737252202d
elyas <neuro.emami@gmail.com>
2026-08-18 18:18:59 -0700

    projects endpoint updated

---
+++ server/src/projects/project.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { z} from "zod";
+
+import { createProject, getProjectDetails, projectStatus } from "./projects.service.ts";
+
+
+const masterSchema = z.object({
+    name : z.string().min(2).max(25),
+    description: z.string().min(1).max(1000),
+    status: z.enum(projectStatus),
+    currentOwnerId: z.string(),
+    createdBy: z.string()
+})
+
+const router = Router();
+
+router.post("/newproject", async(req, res) =>{
+    const parsed = masterSchema.safeParse(req.body);
+    if(!parsed.success)
+        return res.status(400).json({error: parsed.error.flatten()});
+    
+    const newProject = await getProjectDetails(parsed.data.name, parsed.data.description
+        , parsed.data.status, parsed.data.currentOwnerId, parsed.data.createdBy
+    );
+    const project = await createProject(newProject);
+    if (!project) throw new Error(
+        "There was a problem in New Project Creation."
+    );
+    res.status(201).json({message: "Project Created!"});
+});
+export default router;
--- server/src/projects/projects.service.ts
+++ server/src/projects/projects.service.ts
@@ -1,11 +1,32 @@
 import "dotenv/config";
-import { ProjectStatus, type NewProject, type Project } from "./projects.repository.ts";
+import { ProjectStatus, type NewProject } from "./projects.repository.ts";
 import { insertProject } from "./projects.repository.ts";
 
+export const projectStatus = {
+    Active: "active",
+  Inactive: "inactive",
+  Completed: "completed",
+  Cancelled: "cancelled",
+} as const satisfies Record<string, ProjectStatus>;
 
+export async function getProjectDetails(name:string, description:string, status:ProjectStatus,
+    currentOwnerId:string, createdBy:string
+):Promise<NewProject>{
+       const project:NewProject = {
+            name:name,
+            description: description,
+            status:status ?? "active",
+            currentOwnerId:currentOwnerId,
+            createdBy:createdBy
+        }  
+        return project;
+}
 export async function createProject(newProject:NewProject): 
 Promise<NewProject> 
-    {
-        const project = insertProject(newProject);
-        return newProject;
+    {   
+        const project = await getProjectDetails(newProject.name, newProject.description,
+            newProject.status, newProject.currentOwnerId, newProject.createdBy);
+        return insertProject(project);
     }
+
+
